fa3b63bdcc26fe8c135dc4416183ea9762cf198a
Tanveer Ahmad <tanveer8268335@gmail.com>
2025-03-04 23:08:31 +0500

    restarting from the strach of auth page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,17 +1,7 @@
-import AuthForm from "@/components/AuthForm";
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import React from "react";
 
 function AuthPage() {
-  return (
-    <Flex alignItems={"center"} justifyContent={"center"} minH={"100vh"} px={4}>
-      {/* left side  */}
-      <Box display={{ base: "none", md: "block" }}>
-        <Image src="./public/auth.png" height={650} alt="auth image" />
-      </Box>
-
-      <AuthForm />
-    </Flex>
-  );
+  return <div>AuthPage</div>;
 }
 
 export default AuthPage;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,7 @@
-const HomePage = () => {
+import React from "react";
+
+function HomePage() {
   return <div>HomePage</div>;
-};
+}
 
 export default HomePage;
