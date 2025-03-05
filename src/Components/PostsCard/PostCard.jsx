fadd2ccf3a762a1bd8a2f340f374c421c87a7e33
Tanveer Ahmad <tanveer8268335@gmail.com>
2025-03-05 12:02:26 +0500

    shimmer effect added in cards

--- src/Components/PostsCard/PostCard.jsx
+++ src/Components/PostsCard/PostCard.jsx
@@ -1,18 +1,49 @@
-import { Box, Image } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  Image,
+  Skeleton,
+  SkeletonCircle,
+  Stack,
+} from "@chakra-ui/react";
 import PostHeader from "./PostHeader";
 import PostFooter from "./PostFooter";
+import { useEffect, useState } from "react";
 
 function PostCard({ authorName, img, likes, comments, description }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <Box w={"full"} my={5}>
-      <PostHeader authorName={authorName} img={img} />
-      <Image src={img} borderRadius={4} />
-      <PostFooter
-        likes={likes}
-        authorName={authorName}
-        description={description}
-        comments={comments}
-      />
+      {isLoading && (
+        <Box>
+          <HStack gap="5">
+            <SkeletonCircle size="12" />
+            <Stack flex="1">
+              <Skeleton height="5" width="100%" />
+            </Stack>
+          </HStack>
+          <Skeleton height="400px" width="100%" mt={2} />
+        </Box>
+      )}
+      {!isLoading && (
+        <>
+          <PostHeader authorName={authorName} img={img} />
+          <Image src={img} borderRadius={4} />
+          <PostFooter
+            likes={likes}
+            authorName={authorName}
+            description={description}
+            comments={comments}
+          />
+        </>
+      )}
     </Box>
   );
 }
