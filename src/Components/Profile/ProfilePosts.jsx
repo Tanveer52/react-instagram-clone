047eb93de3ecb6dda443b4995b49e4b9aff03762
Tanveer Ahmad <tanveer8268335@gmail.com>
2025-03-05 14:25:10 +0500

    profile page posts, and skeleton loading added

--- src/Components/Profile/ProfilePosts.jsx
+++ src/Components/Profile/ProfilePosts.jsx
@@ -1,7 +1,42 @@
-import { BsBookmark, BsGrid3X3, BsSuitHeart } from "react-icons/bs";
+import { Grid, Image, Skeleton, Stack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 function ProfilePosts() {
-  return <div>ProfilePosts</div>;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 2000);
+  }, []);
+  return (
+    <Grid
+      mt={5}
+      ml={{ base: "4", md: "0" }}
+      gap={5}
+      my={5}
+      templateColumns={{
+        sm: "repeat(1,1fr)",
+        md: "repeat(3,1fr)",
+      }}
+    >
+      {isLoading &&
+        [0, 1, 2, 3, 4, 5].map((item, index) => (
+          <Stack key={index} gap="5">
+            <Skeleton height="300px" />
+          </Stack>
+        ))}
+      {!isLoading &&
+        [1, 2, 3, 4].map((item, index) => (
+          <Image
+            key={index}
+            height={300}
+            width={"full"}
+            fit={"cover"}
+            borderRadius={4}
+            src={`../public/img${item}.png`}
+          />
+        ))}
+    </Grid>
+  );
 }
 
 export default ProfilePosts;
